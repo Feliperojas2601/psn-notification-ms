f68bbda4a3af46e914cc5ebb4f91ce3fc61e44df
Felipe Rojas <jrojasce@unal.edu.co>
2023-03-20 21:40:22 -0500

    rabbit fixed

--- src/main.ts
+++ src/main.ts
@@ -7,27 +7,7 @@ import { ConfigurationService } from './configuration/configuration.service';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configurationService = app.get(ConfigurationService);
-
-  const user = configurationService.get('RABBITMQ_USER');
-  const password = configurationService.get('RABBITMQ_PASSWORD');
-  const host = configurationService.get('RABBITMQ_HOST');
-  const queueName = configurationService.get('RABBITMQ_QUEUE_NAME');
-
   const port = configurationService.get('PORT');
-
-  await app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.RMQ,
-    options: {
-      urls: [`amqp://${user}:${password}@${host}`],
-      queue: queueName,
-      noAck: false,
-      queueOptions: {
-        durable: true,
-      },
-    },
-  });
-
-  await app.startAllMicroservices();
   await app.listen(port);
 }
 bootstrap();
